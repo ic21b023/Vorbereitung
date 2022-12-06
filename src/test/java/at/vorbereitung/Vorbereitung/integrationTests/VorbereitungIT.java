58c99f31c3cfc16b3187b27b60bdbb649003b10b
Roman REITER <reiter@live.at>
2022-12-06 16:23:49 +0100

    #1 init project

---
+++ src/test/java/at/vorbereitung/Vorbereitung/integrationTests/VorbereitungIT.java
@@ -0,0 +1,13 @@
+package at.vorbereitung.Vorbereitung.integrationTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VorbereitungIT {
+
+    @Test
+    public void Dummy(){
+        assertTrue(1==1);
+    }
+}
